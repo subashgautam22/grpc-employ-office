90c5167372ed0f1ee186317e40d1ff65f4b182ba
subashgautam22 <78790305+subashgautam22@users.noreply.github.com>
2022-04-27 18:00:01 +0545

    Update office_get.js

--- server/modules/simple_crud/office_detail/methods/office_get.js
+++ server/modules/simple_crud/office_detail/methods/office_get.js
@@ -7,6 +7,7 @@
           response: { status: httpStatus.BAD_REQUEST, message: "read  Failed" },
         };
         const dbResponse = await sql.get(call.request);
+        dbResponse.sort((a, b) => (a.dep_name > b.dep_name ? 1 : -1));
   
         if (dbResponse && dbResponse.length > 0) {
            response.dataList = dbResponse;
